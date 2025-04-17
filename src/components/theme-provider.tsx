02222408872f3d1c5fcac138d25fcc3a1fe60b62
Fernando Tekisa <164080430+FernandoTekisa@users.noreply.github.com>
2025-04-17 17:36:22 +0100

    nabi

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -6,3 +6,12 @@ import { type ThemeProviderProps } from "next-themes";
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
+// "use client";
+
+// import { ThemeProvider as NextThemesProvider } from "next-themes";
+// import { type ThemeProviderProps } from "next-themes";
+
+// export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+// }
